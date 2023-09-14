791ba8ef474a61a58ad2a2d810c6aec9fddfde7c
AswinJagadeeshan <97330821+AswinJagadeeshan@users.noreply.github.com>
2023-09-14 06:17:58 +0530

    Update ManagerService.java

--- src/main/java/com/makeatable/makeatable/services/ManagerService.java
+++ src/main/java/com/makeatable/makeatable/services/ManagerService.java
@@ -1,8 +1,28 @@
 package com.makeatable.makeatable.services;
 
+import com.makeatable.makeatable.model.Manager;
+import com.makeatable.makeatable.repository.ManagerRepo;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class ManagerService {
 
+    public final ManagerRepo managerRepo;
+
+    public ManagerService(ManagerRepo managerRepo) {
+        this.managerepo = managerepo;
+    }
+    public List<Manager> getAllManager() {
+        return managerRepo.findAll();
+    }
+
+    public Manager addManager(Manager manager) {
+        return managerRepo.save(manager);
+    }
+
+    public Manager updateManager(Manager manager){
+
+    }
 }
