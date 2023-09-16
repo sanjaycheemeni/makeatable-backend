06c1b8341e1146c74bb530448fca67ba4357e3c4
Aashish <aashishanil530@gmail.com>
2023-09-16 11:08:50 +0530

    update manager
    update

--- src/main/java/com/makeatable/makeatable/controller/ManagerController.java
+++ src/main/java/com/makeatable/makeatable/controller/ManagerController.java
@@ -1,4 +1,51 @@
 package com.makeatable.makeatable.controller;
 
 public class ManagerController {
+<<<<<<< Updated upstream
+=======
+    private final ManagerService managerService;
+
+    @GetMapping("/list")
+    public ResponseEntity<Response> getAllManager() {
+        return ResponseEntity.ok(
+                Response.builder()
+                        .timeStamp(now())
+                        .data(of("", managerService.getAllManager()))
+                        .message("list of all managers")
+                        .status(OK)
+                        .statusCode(OK.value())
+                        .build());
+
+    }
+
+    @PostMapping("/create")
+    @ResponseBody
+    public ResponseEntity<Response> addManager(@RequestBody Manager manager) {
+        return ResponseEntity.ok(
+                Response.builder()
+                        .timeStamp(now())
+                        .data(of("Added Manager", managerService.addManager(manager)))
+                        .message("Manager added")
+                        .status(OK)
+                        .statusCode(OK.value())
+                        .build()
+
+
+        );
+    }
+
+
+        public  ResponseEntity<Response> updatemanager(@RequestBody Manager manager, @PathVariable("id") Long id){
+        return ResponseEntity.ok(
+                Response.builder()
+                        .timeStamp(now())
+                        .data(of("Updated manager", managerService.updateManager( id,manager)))
+                        .message("manager updated")
+                        .status(OK)
+                        .statusCode(OK.value())
+                       .build()
+
+        );
+    }
+>>>>>>> Stashed changes
 }
